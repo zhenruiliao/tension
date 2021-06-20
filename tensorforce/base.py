df27cda61cb6f461dee2b02488fb5e927b663afa
lubin-liu <69323872+lubin-liu@users.noreply.github.com>
2021-06-20 15:39:09 -0400

    Update base.py

--- tensorforce/base.py
+++ tensorforce/base.py
@@ -1,14 +1,28 @@
 import numpy as np
-
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import backend, activations
 
 class FORCELayer(keras.layers.AbstractRNNCell):
-    def __init__(self, units, output_size, activation, **kwargs):
+    def __init__(self, units, output_size, activation, seed = None, g = 1.5, 
+                 input_kernel_trainable = False, recurrent_kernel_trainable = False, 
+                 output_kernel_trainable = True, feedback_kernel_trainable = False, **kwargs):
+      
         self.units = units 
-        self._output_size = output_size
+        self.__output_size__ = output_size
         self.activation = activations.get(activation)
+
+        if seed is None:
+          self.__seed_gen__ = tf.random.Generator.from_non_deterministic_state()
+        else:
+          self.__seed_gen__ = tf.random.Generator.from_seed(seed)
+        
+        self.__g__ = g
+
+        self.__input_kernel_trainable__ = input_kernel_trainable
+        self.__recurrent_kernel_trainable__ = recurrent_kernel_trainable
+        self.__feedback_kernel_trainable__ = feedback_kernel_trainable
+        self.__output_kernel_trainable__ = output_kernel_trainable
 
         super().__init__(**kwargs)
 
@@ -18,34 +32,54 @@ class FORCELayer(keras.layers.AbstractRNNCell):
 
     @property 
     def output_size(self):
-        return self._output_size
+        return self.__output_size__
 
     def build(self, input_shape):
- 
+
         self.input_kernel = self.add_weight(shape=(input_shape[-1], self.units),
-                                    initializer='uniform', trainable=False,
-                                    name='input_kernel')
+          initializer=keras.initializers.RandomNormal(mean=0., 
+                                                      stddev= 1/input_shape[-1]**0.5, 
+                                                      seed=self.__seed_gen__.uniform([1], 
+                                                                                 minval=None, 
+                                                                                 dtype=tf.dtypes.int64)[0]),
+          trainable=self.__input_kernel_trainable__,
+          name='input_kernel')
+ 
         self.recurrent_kernel = self.add_weight(
           shape=(self.units, self.units),
-          initializer='uniform', 
-          trainable=False,
+          initializer= keras.initializers.RandomNormal(mean=0., 
+                                                       stddev= self.__g__/self.units**0.5, 
+                                                       seed=self.__seed_gen__.uniform([1], 
+                                                                                  minval=None, 
+                                                                                  dtype=tf.dtypes.int64)[0]), 
+          trainable=self.__recurrent_kernel_trainable__,
           name='recurrent_kernel')
+        
         self.feedback_kernel = self.add_weight(
           shape=(self.output_size, self.units), 
-          initializer='uniform', 
-          trainable=False,
+          initializer=keras.initializers.RandomNormal(mean=0., 
+                                                      stddev= 1, 
+                                                      seed=self.__seed_gen__.uniform([1], 
+                                                                                 minval=None, 
+                                                                                 dtype=tf.dtypes.int64)[0]), 
+          trainable=self.__feedback_kernel_trainable__,
           name='feedback_kernel')
+        
         self.output_kernel = self.add_weight(
           shape=(self.units, self.output_size),
-          initializer='uniform', trainable=True,
+          initializer=keras.initializers.RandomNormal(mean=0., 
+                                                      stddev= 1/self.units**0.5, 
+                                                      seed=self.__seed_gen__.uniform([1], 
+                                                                                 minval=None, 
+                                                                                 dtype=tf.dtypes.int64)[0]), 
+          trainable=self.__output_kernel_trainable__,
           name='output_kernel')      
 
         self.built = True
 
     @classmethod
     def from_weights(cls, weights, **kwargs):
-        """Initialize the network from a list of weights (i.e., user-generated)"""
-
+        # Initialize the network from a list of weights (e.g., user-generated)
         input_kernel, recurrent_kernel, feedback_kernel, output_kernel = weights 
         input_shape, input_units = input_kernel.shape 
         recurrent_units1, recurrent_units2 = recurrent_kernel.shape 
@@ -60,24 +94,28 @@ class FORCELayer(keras.layers.AbstractRNNCell):
         assert feedback_output_size == output_size 
 
         self = cls(units=units, output_size=output_size, **kwargs)
+
         self.input_kernel = self.add_weight(shape=(input_shape, self.units),
                                       initializer=keras.initializers.constant(input_kernel),
-                                      trainable = False,
+                                      trainable = self.__input_kernel_trainable__,
                                       name='input_kernel')
+        
         self.recurrent_kernel = self.add_weight(
             shape=(self.units, self.units),
             initializer=keras.initializers.constant(recurrent_kernel),
-            trainable = False,
+            trainable = self.__recurrent_kernel_trainable__,
             name='recurrent_kernel')
+        
         self.feedback_kernel = self.add_weight(
             shape=(self.output_size, self.units),
             initializer=keras.initializers.constant(feedback_kernel),
-            trainable = False,
+            trainable = self.__feedback_kernel_trainable__,
             name='feedback_kernel')
+        
         self.output_kernel = self.add_weight(
             shape=(self.units, self.output_size),
             initializer=keras.initializers.constant(output_kernel),
-            trainable = True,
+            trainable = self.__output_kernel_trainable__,
             name='output_kernel')      
 
         self.built = True
@@ -88,33 +126,46 @@ class FORCEModel(keras.Model):
     def __init__(self, force_layer, alpha_P=1.,return_sequences=True):
         super().__init__()
         self.alpha_P = alpha_P
-
         self.force_layer = keras.layers.RNN(force_layer, 
                                             stateful=True, 
                                             return_state=True, 
                                             return_sequences=return_sequences)
 
         self.units = force_layer.units 
- 
+
+        self.__force_layer__ = force_layer
  
 
     def build(self, input_shape):
-
-        self.P = self.add_weight(name='P', shape=(self.units, self.units), 
+        self.P_output = self.add_weight(name='P_output', shape=(self.units, self.units), 
                                  initializer=keras.initializers.Identity(
                                               gain=self.alpha_P), trainable=True)
+        
         super().build(input_shape)
- 
+
 
     def call(self, x, training=False,   **kwargs):
-        #x = self.input_layer(x)
+
         if training:
             return self.force_layer(x, **kwargs) 
         else:
-            return self.force_layer(x, **kwargs)[0] 
+            initialization = all(v is None for v in self.force_layer.states)
+            
+            if not initialization:
+              original_state = [i.numpy() for i in self.force_layer.states]
+            output = self.force_layer(x, **kwargs)[0] 
+
+            if not initialization:
+              self.force_layer.reset_states(states = original_state)
+            return output
 
     def train_step(self, data):
+
         x, y = data
+
+        if self.run_eagerly:
+          self.hidden_activation = []
+        
         for i in range(x.shape[1]):
           z, _, h, _ = self(x[:,i:i+1,:], training=True)
 
@@ -123,53 +174,76 @@ class FORCEModel(keras.Model):
 
           # Compute pseudogradients
           trainable_vars = self.trainable_variables
-          dP = self.__pseudogradient_P(h, z, y[:,i,:])
+          for idx in range(len(trainable_vars)):
+            if 'output_kernel' in trainable_vars[idx].name:
+              output_kernel_idx = idx
+            elif 'P_output' in trainable_vars[idx].name:
+              P_output_idx = idx
 
+          dP = self.__pseudogradient_P(h, z, y[:,i,:])
           # Update weights
-          self.optimizer.apply_gradients(zip([dP], [trainable_vars[1]]))
+          self.optimizer.apply_gradients(zip([dP], [trainable_vars[P_output_idx]]))
           
           dwO = self.__pseudogradient_wO(h, z, y[:,i,:])
-          
-          # Update weights
-          self.optimizer.apply_gradients(zip([dwO], [trainable_vars[0]]))
+          self.optimizer.apply_gradients(zip([dwO], [trainable_vars[output_kernel_idx]]))
 
         # Update metrics (includes the metric that tracks the loss)
           self.compiled_metrics.update_state(y[:,i,:], z)
         # Return a dict mapping metric names to current value
 
+          if self.run_eagerly:
+            self.hidden_activation.append(h.numpy()[0])
+
         return {m.name: m.result() for m in self.metrics}
 
-
     def __pseudogradient_P(self, h, z, y):
-        """Implements the training step i.e. the rls() function
-        This not a real gradient (does not use gradient.tape())
-        Computes the actual update"""  
+        # Implements the training step i.e. the rls() function
+        # This not a real gradient (does not use gradient.tape())
+        # Computes the actual update
+        # Example array shapes
+        # h : 1 x 500
+        # P : 500 x 500 
+        # k : 500 x 1 
+        # hPht : 1 x 1
+        # dP : 500 x 500 
 
-        k = backend.dot(self.P, tf.transpose(h))
+
+        k = backend.dot(self.P_output, tf.transpose(h))
         hPht = backend.dot(h, k)
         c = 1./(1.+hPht)
         assert c.shape == (1,1)
-        hP = backend.dot(h, self.P)
+        hP = backend.dot(h, self.P_output)
         dP = backend.dot(c*k, hP)
         
-        return dP 
+        return  dP 
 
     def __pseudogradient_wO(self, h, z, y):
+        # z : 1 x 20 
+        # y : 1 x 20
+        # e : 1 x 20
+        # dwO : 500 x 20  
 
         e = z-y
-        Ph = backend.dot(self.P, tf.transpose(h))
+        Ph = backend.dot(self.P_output, tf.transpose(h))
         dwO = backend.dot(Ph, e)
 
-        return dwO
+        return  dwO
 
     def compile(self, metrics, **kwargs):
-        super().compile(optimizer=keras.optimizers.SGD(learning_rate=1),  metrics=metrics)
+        super().compile(optimizer=keras.optimizers.SGD(learning_rate=1), loss = 'mae', metrics=metrics,   **kwargs)
 
-    def fit(self, x, y, epochs, verbose, **kwargs):
-        if len(x.shape) < 3:
+    def fit(self, x, y=None, epochs = 1, verbose = 'auto', **kwargs):
+
+        if len(x.shape) < 2 or len(x.shape) > 3:
+            raise ValueError('Shape of x is invalid')
+
+        if len(y.shape) < 2 or len(y.shape) > 3:
+            raise ValueError('Shape of y is invalid')
+        
+        if len(x.shape) == 2:
             x = tf.expand_dims(x, axis = 0)
         
-        if len(y.shape) < 3:
+        if len(y.shape) == 2:
             y = tf.expand_dims(y, axis = 0)
         
         if x.shape[0] != 1:
@@ -177,15 +251,20 @@ class FORCEModel(keras.Model):
 
         if y.shape[0] != 1:
             raise ValueError("Dim 0 of y must be 1")
-        if x.shape[1] != y.shape[1]:
+        
+        if x.shape[1] != y.shape[1]: 
             raise ValueError('Timestep dimension of inputs must match')     
-        super().fit(x = x, y = y, epochs = epochs, batch_size = 1, verbose = verbose, **kwargs)
+
+        return super().fit(x = x, y = y, epochs = epochs, batch_size = 1, verbose = verbose, **kwargs)
 
     def predict(self, x, **kwargs):
         if len(x.shape) == 3 and x.shape[0] != 1:
-            raise ValueError()
+            raise ValueError('Dim 0 must be 1')
         
-        if len(x.shape) < 3:
+        if len(x.shape) < 2 or len(x.shape) > 3:
+            raise ValueError('')
+
+        if len(x.shape) == 2:
             x = tf.expand_dims(x, axis = 0)
-        
-        return self(x, training = False)[0] 
+
+        return self(x, training = False)[0]
