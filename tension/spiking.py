b79765bc8e0e67406426121732abeb2ce61ac7c7
Zhenrui Liao <zhenruiliao@users.noreply.github.com>
2022-08-30 21:05:41 -0400

    Update spiking.py

--- tension/spiking.py
+++ tension/spiking.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import backend, activations
 import numpy as np
-from base import FORCELayer, FORCEModel
+from .base import FORCELayer, FORCEModel
 
 class SpikingNN(FORCELayer):
     """
